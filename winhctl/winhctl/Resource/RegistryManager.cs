e9b005725ff28fc5131c34429fe99d1b6354cf1b
pjdsant@gmail.com <pjdsant@gmail.com>
2019-08-20 09:35:41 -0300

    Add RegMan

---
+++ winhctl/winhctl/Resource/RegistryManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeWater.WinHCtl.Resource
+{
+    public class RegistryManager
+    {
+        public string ReadRegistry(string strKey)
+        {
+            var result = "";
+
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\PJSIT");
+
+            if (key != null)
+            {
+                result = key.GetValue("Clicks").ToString();
+            }
+
+            return result;
+        }
+    }
+}
